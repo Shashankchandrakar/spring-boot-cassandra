af2a4b8d29deafa4a4d8a583c719a63b0f45ad34
Shashank <shashankchandrakarr@gmail.com>
2021-01-26 01:43:10 +0530

    added check in update api:

--- src/main/java/com/football/controllers/PlayerController.java
+++ src/main/java/com/football/controllers/PlayerController.java
@@ -5,6 +5,7 @@ import com.football.service.PlayerService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import javax.validation.Valid;
 import java.util.UUID;
 
 @RestController
@@ -24,7 +25,7 @@ public class PlayerController {
     }
 
     @PostMapping("/create")
-    public Player createPlayer(@RequestBody Player player){
+    public Player createPlayer(@Valid @RequestBody Player player){
         return playerService.createPlayer(player);
     }
 
@@ -34,7 +35,10 @@ public class PlayerController {
     }
 
     @PutMapping("/update/{playerId}")
-    public Player updatePlayer(@RequestBody Player player){
+    public Player updatePlayer(@RequestBody Player player,@PathVariable("playerId") UUID id){
+        if(!player.getPlayerId().equals(id)){
+            throw new RuntimeException("Player uuid and path variable uuid does not match");
+        }
         return playerService.updatePlayer(player);
     }
 }
