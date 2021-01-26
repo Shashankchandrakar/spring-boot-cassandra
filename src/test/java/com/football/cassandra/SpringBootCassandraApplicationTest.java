e38489f087ab422bae71b142f2071bead5c4922d
Shashank <shashankchandrakarr@gmail.com>
2021-01-26 23:44:13 +0530

    remove todo

--- src/test/java/com/football/cassandra/SpringBootCassandraApplicationTest.java
+++ src/test/java/com/football/cassandra/SpringBootCassandraApplicationTest.java
@@ -3,9 +3,10 @@ package com.football.cassandra;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.context.annotation.Import;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
 
-//todo add test here for cassandra
+@ActiveProfiles("test")
 @RunWith(SpringRunner.class)
 @Import({CassandraConfig.class})
 public class SpringBootCassandraApplicationTest {
